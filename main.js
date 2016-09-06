4cd13a8ef4ad4388ccde28efdaa251e1ec904d96
KAKAN <satwiksatpathy@gmail.com>
2016-09-06 18:08:57 +0530

    Removed uneccessary code.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// "÷"
 // Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
 var operators = ['+', '-', 'x', '/'];
@@ -24,7 +23,7 @@ for(var i = 0; i < keys.length; i++) {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			// Replace all instances of x and / with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
 			equation = equation.replace(/x/g, '*');//.replace(/÷/g, '/');
 			
 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
